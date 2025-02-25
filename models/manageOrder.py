e284741df521fd46b242e5437717e51802503d68
Le Quang Truong <126086750+quangtruong3011@users.noreply.github.com>
2025-02-25 14:45:38 +0700

    format code

--- models/manageOrder.py
+++ models/manageOrder.py
@@ -2,22 +2,26 @@ from models.order import Order
 
 
 class ManageOrder:
-    #Khởi tạo đối tượng quản lý đơn hàng.
+    # Khởi tạo đối tượng quản lý đơn hàng.
     def __init__(self):
         self.orders = []
-    #Thêm một đơn hàng mới vào danh sách.
+
+    # Thêm một đơn hàng mới vào danh sách.
     def add_order(self, order):
-        if not order.order_id or (order.order_id in [order.order_id for order in self.orders]):
-            print ("Mã đơn hàng không hợp lệ hoặc đã tồn tại.")
+        if not order.order_id or (
+            order.order_id in [order.order_id for order in self.orders]
+        ):
+            print("Mã đơn hàng không hợp lệ hoặc đã tồn tại.")
         elif order.customer_name == "":
-            print ("Tên khách hàng không được để trống.")
+            print("Tên khách hàng không được để trống.")
         elif order.phone == "" and not order.phone.isdigit():
-            print ("Số điện thoại không hợp lệ.")
+            print("Số điện thoại không hợp lệ.")
         elif order.address == "":
-            print ("Địa chỉ không được để trống.")
+            print("Địa chỉ không được để trống.")
         else:
             self.orders.append(order)
-    #Chỉnh sửa thông tin đơn hàng dựa trên mã đơn hàng.
+
+    # Chỉnh sửa thông tin đơn hàng dựa trên mã đơn hàng.
     def edit_order(
         self,
         order_id,
@@ -40,32 +44,44 @@ class ManageOrder:
                     product_name, new_name, new_quantity, new_price = product_data
                     for product in order.products:
                         if product.name == product_name:
-                            product.edit_product(new_name or None, new_quantity or None, new_price or None)
+                            product.edit_product(
+                                new_name or None,
+                                new_quantity or None,
+                                new_price or None,
+                            )
                             break
                         else:
-                            print ("Sản phẩm không tồn tại.")
+                            print("Sản phẩm không tồn tại.")
                 order.total_amount = order.calculate_total_amount()
-            if new_status and new_status in [Order.STATUS_PENDING, Order.STATUS_DELIVERING, Order.STATUS_DELIVERED]:
+            if new_status and new_status in [
+                Order.STATUS_PENDING,
+                Order.STATUS_DELIVERING,
+                Order.STATUS_DELIVERED,
+            ]:
                 order.update_status(new_status)
         else:
-            print ("Đơn hàng không tồn tại.")
-    #Xoá đơn hàng khỏi danh sách.
+            print("Đơn hàng không tồn tại.")
+
+    # Xoá đơn hàng khỏi danh sách.
     def delete_order(self, order_id):
         order = self.find_order_by_id(order_id)
         if order:
             self.orders.remove(order)
         else:
-            print ("Đơn hàng không tồn tại.")
-    #Tìm đơn hàng theo mã đơn hàng.
+            print("Đơn hàng không tồn tại.")
+
+    # Tìm đơn hàng theo mã đơn hàng.
     def find_order_by_id(self, order_id):
         for order in self.orders:
             if order.order_id == order_id:
                 return order
         return None
-    #Tìm đơn hàng theo số điện thoại.
+
+    # Tìm đơn hàng theo số điện thoại.
     def find_order_by_phone(self, phone):
         return [order for order in self.orders if order.phone == phone]
-    #Danh sách các đơn hàng theo trạng thái đơn hàng.
+
+    # Danh sách các đơn hàng theo trạng thái đơn hàng.
     def display_orders_by_status(self, status):
         if not self.orders:
             return []
@@ -74,9 +90,10 @@ class ManageOrder:
             if order.status == status:
                 order_status.append(order)
         if len(order_status) == 0:
-            print("Không có đơn hàng nào ở trạng thái",status)
+            print("Không có đơn hàng nào ở trạng thái", status)
         return order_status
-    #Tính tổng doanh thu các đơn hàng đã giao.
+
+    # Tính tổng doanh thu các đơn hàng đã giao.
     def calculate_total_revenue(self):
         return sum(
             [
@@ -85,10 +102,11 @@ class ManageOrder:
                 if order.status == Order.STATUS_DELIVERED
             ]
         )
-    #Sắp xếp danh sách đơn hàng theo tổng tiền.
+
+    # Sắp xếp danh sách đơn hàng theo tổng tiền.
     def sort_orders_by_total_amount(self, ascending=True):
         if not self.orders:
-            print ("Danh sách đơn hàng rỗng.")
+            print("Danh sách đơn hàng rỗng.")
             return []
         else:
             return sorted(
@@ -96,15 +114,21 @@ class ManageOrder:
                 key=lambda order: (order.total_amount, order.order_id),
                 reverse=not ascending,
             )
-    #Danh sách các đơn hàng ở trạng thái "Chờ xử lý" và "Đang giao".
+
+    # Danh sách các đơn hàng ở trạng thái "Chờ xử lý" và "Đang giao".
     def get_pending_orders(self):
-        if not self.orders and not self.display_orders_by_status(Order.STATUS_PENDING) and not self.display_orders_by_status(Order.STATUS_DELIVERING):
-            print ("Danh sách đơn hàng rỗng.")
+        if (
+            not self.orders
+            and not self.display_orders_by_status(Order.STATUS_PENDING)
+            and not self.display_orders_by_status(Order.STATUS_DELIVERING)
+        ):
+            print("Danh sách đơn hàng rỗng.")
             return []
         return self.display_orders_by_status(
             Order.STATUS_PENDING
         ) + self.display_orders_by_status(Order.STATUS_DELIVERING)
-    #Danh sách các sản phẩm bán chạy nhất
+
+    # Danh sách các sản phẩm bán chạy nhất
     def get_top_selling_products(self, top_n=5):
         products_sales = {}
         for order in self.orders:
@@ -116,11 +140,11 @@ class ManageOrder:
                         products_sales[product.name] = product.quantity
         sorted_products = sorted(
             products_sales.items(), key=lambda x: x[1], reverse=True
-        )   
+        )
         if len(sorted_products) == 0:
-            print ("Không có sản phẩm nào được bán.")
+            print("Không có sản phẩm nào được bán.")
             return []
         elif top_n > len(sorted_products):
             return sorted_products[:top_n]
-        else: 
+        else:
             return sorted_products
--- models/product.py
+++ models/product.py
@@ -1,14 +1,16 @@
 class Product:
-    #Khởi tạo đối tượng sản phẩm.
+    # Khởi tạo đối tượng sản phẩm.
     def __init__(self, name: str, quantity: int, price: float):
-        self.name = name         #Tên sản phẩm
-        self.quantity = quantity #Số lượng sản phẩm
-        self.price = price       #Đơn giá sản phẩm
-    #Tính tổng giá trị sản phẩm (số lượng * giá).
+        self.name = name  # Tên sản phẩm
+        self.quantity = quantity  # Số lượng sản phẩm
+        self.price = price  # Đơn giá sản phẩm
+
+    # Tính tổng giá trị sản phẩm (số lượng * giá).
     def get_total_price(self):
         return self.quantity * self.price
-    #Chỉnh sửa thông tin sản phẩm.
-    def edit_product(self, new_name = None, new_quantity = None, new_price = None):
+
+    # Chỉnh sửa thông tin sản phẩm.
+    def edit_product(self, new_name=None, new_quantity=None, new_price=None):
         if new_name:
             self.name = new_name
         if new_quantity:
@@ -18,12 +20,13 @@ class Product:
                 print("Số lượng phải lớn hơn 0.")
         if new_price:
             try:
-                new_price=float(new_price)
+                new_price = float(new_price)
                 if new_price > 0:
                     self.price = new_price
             except:
                 print("Đơn giá không hợp lệ.")
-    #Trả về chuỗi biểu diễn của sản phẩm.
+
+    # Trả về chuỗi biểu diễn của sản phẩm.
     def __str__(self):
         return (
             f"{self.name} - {self.quantity} x  {self.price} = {self.get_total_price()}"
